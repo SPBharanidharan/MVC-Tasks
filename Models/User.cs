20d69b2319dcba68b676be68387e5d08e677dd42
SPBharanidharan <bharanispb2002@gmail.com>
2025-08-25 08:44:53 +0530

    initial commit

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace BankingApplication.Data
+{
+    public partial class User
+    {
+
+        public int Id { get; set; }
+
+
+        public string Username { get; set; } = null!;
+
+        public string PasswordHash { get; set; } = null!;
+
+        public string? PinHash { get; set; }
+
+        public virtual ICollection<Account> Accounts { get; set; } = new List<Account>();
+    }
+}
